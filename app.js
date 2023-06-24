cde3c7d86e0c2ebdc99cc7fe23f50d8e954a3fa9
MarwanOladele <oladelemarwan@gmail.com>
2023-06-24 20:11:45 +0100

    database connected

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+// asysc errors
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
+
+// connect DB
+const connectDB = require("./db/connect");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products</a>');
+});
+
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Connecting to port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
